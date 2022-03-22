c51303ba248c572ffa3ed527f48544fe06d3d67c
유민기 <mgyou@nextree.io>
2022-03-23 08:41:50 +0900

    3.0 Form and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //function component
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+    // event 안에 있는 target 안에 있는 value를 달라고 하는 것
+    const onChange = (event) => {
+        const {
+          target: { value },
+        } = event;
+        setNweet(value);
+    }
+    return (
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            value={nweet}
+            onChange={onChange}
+            type="text"
+            placeholder="What's on your mind?"
+            maxLength={120}
+          />
+          <input type="submit" value="Nweet" />
+        </form>
+      </div>
+    );
+};
 export default Home;
