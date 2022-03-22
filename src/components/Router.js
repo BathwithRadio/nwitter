1b1291f13f1fdd02397aa0a7792632bdc9961734
유민기 <mgyou@nextree.io>
2022-03-22 23:21:29 +0900

    2.6 Log out Redirect, useHistory

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,22 +2,34 @@ import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
+import { Redirect } from "react-router-dom";
 
 const AppRouter = ({isLoggedIn}) => { // 왜 Destructuring이 필요하지??
   //Hooks
   return (
     <Router>
+      {isLoggedIn && <Navigation/>}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
                 <Home />
             </Route>
+            <Route exact path="/Profile">
+                <Profile />
+            </Route>
+             {/* "/"이 route에 있으면 상관 없는데  그 외의 route로 가게되면 "/"로 돌아가라는 뜻*/}
+            {/* <Redirect from="*" to="/" /> */}
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            {/* <Redirect from="*" to="/" /> */}
+          </>
         )}
       </Switch>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,19 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 //function component
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+    // Hook 사용
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    };
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    )
+};
 export default Profile;
