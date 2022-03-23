b14438a2c7c7c804d6d64e712cdd45c986cc1f89
유민기 <mgyou@nextree.io>
2022-03-23 09:11:34 +0900

    3.1 Nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import { dbService } from "fbase";
 import React, { useState } from "react";
 
 //function component
@@ -5,6 +6,12 @@ const Home = () => {
     const [nweet, setNweet] = useState("");
     const onSubmit = (event) => {
         event.preventDefault();
+        dbService.collection("nweets").add({
+            // nweet key에 nweet value를 넣는다
+            nweet,
+            createdAt: Date.now(),
+        });
+        setNweet("");
     }
     // event 안에 있는 target 안에 있는 value를 달라고 하는 것
     const onChange = (event) => {
