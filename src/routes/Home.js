fcd53561a3d711e12ab532ad44a725c36a2f4e45
유민기 <mgyou@nextree.io>
2022-03-23 22:01:58 +0900

    4.0 Preview Image part 1

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,64 +4,81 @@ import React, { useEffect, useState } from "react";
 
 //function component
 const Home = ({ userObj }) => {
-    const [nweet, setNweet] = useState("");
-    const [nweets, setNweets] = useState([]);
-    //
-    useEffect(() => {
-        // onSnapshot Listner추가 - 데이터베이스에 뭔가(CRUD)를 하게 되면 알 수 있도록 해줌
-        // 결국 nweets는 우리가 페이지를 불러올 때 snapshot에서 나오는 것
-        dbService.collection("nweets").onSnapshot((snapshot) => {
-          const nweetArray = snapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
-          setNweets(nweetArray);
-        });
-    }, []);
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  //
+  useEffect(() => {
+    // onSnapshot Listner추가 - 데이터베이스에 뭔가(CRUD)를 하게 되면 알 수 있도록 해줌
+    // 결국 nweets는 우리가 페이지를 불러올 때 snapshot에서 나오는 것
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
+  }, []);
 
-    //
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        await dbService.collection("nweets").add({
-            text: nweet,
-            createdAt: Date.now(),
-            creatorId: userObj.uid,
-        });
-        setNweet("");
-    }
+  //
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setNweet("");
+  };
 
-    // event 안에 있는 target 안에 있는 value를 달라고 하는 것
-    const onChange = (event) => {
-        const {
-          target: { value },
-        } = event;
-        setNweet(value);
+  // event 안에 있는 target 안에 있는 value를 달라고 하는 것
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  // 사진 입력시 변화를 주도록
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    // 파일을 가지고
+    const theFile = files[0];
+    // reader를 만들어서
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      console.log(finishedEvent);
     }
-    
-    return (
+    // readAsDataURL를 이용해서 파일을 읽는 것
+    reader.readAsDataURL(theFile);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="file" accept="image/*" onChange={onFileChange} />
+        <input type="submit" value="Nweet" />
+      </form>
       <div>
-        <form onSubmit={onSubmit}>
-          <input
-            value={nweet}
-            onChange={onChange}
-            type="text"
-            placeholder="What's on your mind?"
-            maxLength={120}
+        {nweets.map((nweet) => (
+          //Nweet은 내가 만든 Component이고
+          <Nweet
+            key={nweet.id}
+            //NweetObj는 전체의 Object이다 - 그리고 그것을 prop처럼 보내준다.
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
           />
-          <input type="submit" value="Nweet" />
-        </form>
-        <div>
-          {nweets.map((nweet) => (
-            //Nweet은 내가 만든 Component이고
-            <Nweet
-              key={nweet.id}
-              //NweetObj는 전체의 Object이다 - 그리고 그것을 prop처럼 보내준다.
-              nweetObj={nweet}
-              isOwner={nweet.creatorId === userObj.uid}
-            />
-          ))}
-        </div>
+        ))}
       </div>
-    );
+    </div>
+  );
 };
 export default Home;
